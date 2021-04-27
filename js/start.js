820a9f0df157d2615556f598e8650a654abd7a44
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-04-27 16:02:58 +0900

    qna구현(innerHTML, createElement)

--- js/start.js
+++ js/start.js
@@ -1,6 +1,21 @@
 const main = document.querySelector('#main'); // const : 변수를 상수로 선언하여 1개만 선언가능
 const qna = document.querySelector('#qna'); // querySelector : 문서에서 css 선택자에 대응되는것을 선택해줌
 
+function addAnswer(answerText) {
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+}
+
+function goNext(qIdx) {
+  var q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer);
+  }
+}
+
 function begin() {
   main.style.WebkitAnimation = 'fadeOut 1s';
   main.style.animation = 'fadeOut 1s';
@@ -12,5 +27,7 @@ function begin() {
       main.style.display = 'none';
       qna.style.display = 'block';
     }, 450);
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
