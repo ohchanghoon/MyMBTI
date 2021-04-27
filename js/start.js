20a6507b98f1d637423cc09624d209d94a157142
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-04-27 16:12:38 +0900

    다음질문으로 넘어가기 (addeventListener)

--- js/start.js
+++ js/start.js
@@ -1,18 +1,31 @@
 const main = document.querySelector('#main'); // const : 변수를 상수로 선언하여 1개만 선언가능
 const qna = document.querySelector('#qna'); // querySelector : 문서에서 css 선택자에 대응되는것을 선택해줌
 
-function addAnswer(answerText) {
+function addAnswer(answerText, qIdx) {
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
+  answer.classList.add('answerList');
   a.appendChild(answer);
   answer.innerHTML = answerText;
+  answer.addEventListener(
+    'click',
+    function () {
+      var children = document.querySelectorAll('.answerList');
+      for (let i = 0; i < children.length; i++) {
+        children[i].disabled = true;
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },
+    false
+  );
 }
 
 function goNext(qIdx) {
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for (let i in qnaList[qIdx].a) {
-    addAnswer(qnaList[qIdx].a[i].answer);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
 }
 
